800d1a5eadc4de744d767e683cfa8313d101cb0a
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-04-23 14:00:59 +0300

    Create CustomButton.jsx

---
+++ components/CustomButton.jsx
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyle,
+  textStyles,
+  isLoading,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[50px] justify-center items-center ${containerStyle} ${
+        isLoading ? `opacity-50` : ``
+      }`}
+    >
+      <Text className={`text-primary text-lg font-psemibold ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
